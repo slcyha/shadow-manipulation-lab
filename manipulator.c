f3fda76ab537d193a3a667c6d905fcc6d5848376
yasemin nazlı göktaş <slycha.nyx@gmail.com>
2026-02-10 13:25:58 +0300

    Add manipulator.c to demonstrate pointer usage

---
+++ manipulator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() ;
+    int hedef_veri = 100; 
+    int *ptr = &hedef_veri;
+
+    printf("[!]  Değer: %d\n", hedef_veri);
+
+    printf("[*] Hedefin Bellek Adresi: %p\n", (void*)ptr);
+
+    printf("\ngerçekleşiyor\n\n");
+    *ptr = 500; 
+
+    printf("[+] Yeni Değer: %d\n", hedef_veri);
+
+    return 0;
+}
