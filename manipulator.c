4e1f76bbbabf234d7105a52416cb87f07a2288af
Yasemin Nazlı Göktaş <slycha.nyx@gmail.com>
2026-07-23 18:44:46 +0300

    Refactor main function in manipulator.c

--- manipulator.c
+++ manipulator.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
-int main() ;
+int main() {
     int hedef_veri = 100; 
     int *ptr = &hedef_veri;
-
     printf("[!]  Değer: %d\n", hedef_veri);
-
     printf("[*] Hedefin Bellek Adresi: %p\n", (void*)ptr);
-
     printf("\ngerçekleşiyor\n\n");
     *ptr = 500; 
-
     printf("[+] Yeni Değer: %d\n", hedef_veri);
-
     return 0;
 }
